73491ee0ec1fae504a535cfc85679b0acf360c15
Kauê de Magalhães <kauedemagalhaes27@gmail.com>
2023-11-17 16:23:22 -0300

    chore: learning to use shadcn by using the button

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
-import Image from 'next/image'
+import { Button } from '@/components/ui/button'
 
 export default function Home() {
   return (
-    <p>Hello Notion Clone</p>
+    <Button variant={'secondary'} size={'lg'}>
+      Click me
+    </Button>
   )
 }
