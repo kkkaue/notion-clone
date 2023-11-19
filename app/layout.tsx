ab76527fb88e3a0810883e9d824631fb8dfe4daf
Kauê de Magalhães <kauedemagalhaes27@gmail.com>
2023-11-19 19:46:40 -0300

    feat: adding database and authentication with Convex and Clerk
    links:
     -https://docs.convex.dev/quickstart/nextjs
    
    -https://clerk.com/docs/integrations/databases/convex

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import { ThemeProvider } from '@/components/providers/theme-provider'
 import { cn } from "@/lib/utils";
+import { ConvexClientProvider } from '@/components/providers/convex-provider';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -33,15 +34,17 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={cn("dark:bg-[#1f1f1f]", inter.className)}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-          storageKey="jotion-theme-2"
-        >
-          {children}
-        </ThemeProvider>
+        <ConvexClientProvider>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+            storageKey="jotion-theme-2"
+          >
+            {children}
+          </ThemeProvider>
+        </ConvexClientProvider>
       </body>
     </html>
   )
---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+    providers: [
+        {
+            domain: "https://flying-sunbird-0.clerk.accounts.dev",
+            applicationID: "convex",
+        },
+    ]
+};
